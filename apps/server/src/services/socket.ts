823f5196144c6720882ff836d33cb4824b513ee8
harikiran713 <harikiranl713@gamil.com>
2025-07-25 13:19:24 +0530

    created a socket server

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,13 @@
+import { Server, Socket } from "socket.io";
+class SocketService{
+    private _io:Server;
+    constructor(){
+        console.log('init socket Service')
+        this._io=new Server()
+    }
+    get io(){
+        return this._io;
+    }
+
+}
+export default SocketService;
